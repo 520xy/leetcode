a8c9a05a7f60b06332a4a1ef288cda2333179bf2
xy <m15696680128@163.com>
2023-02-14 16:24:07 +0800

    leetcode

--- editor/cn/[11]盛最多水的容器.java
+++ editor/cn/[11]盛最多水的容器.java
@@ -1,4 +1,4 @@
-//给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。 
+package editor.cn;//给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
 //
 // 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。 
 //
@@ -38,9 +38,24 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int maxArea(int[] height) {
-
-    }
-}
+//class Solution {
+//    //贪心，双指针，我们只要更高（因为指针往中间靠，长度会越来越短）
+//    public int maxArea(int[] height) {
+//        if(height==null||height.length<2)return 0;
+//        int result=0;
+//        //定义首尾指针开始计算最大值
+//        for(int head=0,tail=height.length-1;head<tail;)
+//            //前面长就移动后面的指针，希望后面也会变高
+//            if(height[head]>=height[tail]) {
+//                result=Math.max(result,(tail-head)*height[tail]);
+//                tail--;
+//            }
+//            //后面长就移动前面的指针，希望前面也会变高
+//            else {
+//                result=Math.max(result,(tail-head)*height[head]);
+//                head++;
+//            }
+//        return result;
+//    }
+//}
 //leetcode submit region end(Prohibit modification and deletion)
